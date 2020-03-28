98d645cbb7c517479b834c23334907602d4c9b4a
Bhavika Soni <bhavikas.inexture@gmail.com>
2020-03-28 12:09:37 +0530

    Created cloud config server, auth and gateway clients with conslu service registry.

---
+++ terabizcloud-config/src/test/java/com/terabizcloud/config/TerabizcloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terabizcloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerabizcloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ terabizcloud-gateway/src/test/java/com/terabizcloud/gateway/terabizcloudgateway/TerabizcloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terabizcloud.gateway.terabizcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerabizcloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
