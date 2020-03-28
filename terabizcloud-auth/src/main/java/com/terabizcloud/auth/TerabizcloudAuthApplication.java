f9a949bf9e0f7d8ca1f2f63b3a85f31e29aa806a
Bhavika Soni <bhavikas.inexture@gmail.com>
2020-03-28 10:45:51 +0530

    Included nectflix zuul.

--- terabizcloud-auth/src/main/java/com/terabizcloud/auth/TerabizcloudAuthApplication.java
+++ terabizcloud-auth/src/main/java/com/terabizcloud/auth/TerabizcloudAuthApplication.java
@@ -3,10 +3,12 @@ package com.terabizcloud.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableZuulProxy
 public class TerabizcloudAuthApplication {
 
 	public static void main(String[] args) {
